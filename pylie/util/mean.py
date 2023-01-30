1dc12d47b3ab5d10a226da0f837d93e971b702f8
Eirik Falck <eirik.falck@sintef.no>
2023-01-30 17:46:50 +0100

    Add docstring to mean functions.

--- pylie/util/mean.py
+++ pylie/util/mean.py
@@ -9,7 +9,16 @@ DEFAULT_MAX_ITERS = 20
 
 def mean(poses: List[Union[SE3, SE2, SO3, SO2]], 
          conv_thresh=DEFAULT_CONV_THRESH, 
-         max_iters=DEFAULT_MAX_ITERS) -> SE3:
+         max_iters=DEFAULT_MAX_ITERS) -> Union[SE3, SE2, SO3, SO2]:
+    """Calculate the mean pose from a list of poses using an iterative 
+    algorithm.
+    
+    :param poses: List of poses
+    :param conv_thresh: Convergence threshold for norm of tangent vector
+    :param max_iters: Maximum iterations used in the algorithm
+    
+    :return: Mean pose of same type as input poses
+    """
     N = len(poses)
     mean_pose = poses[0]
 
@@ -30,13 +39,21 @@ def mean(poses: List[Union[SE3, SE2, SO3, SO2]],
     return mean_pose
 
 
-def mean_SE3_matrix(poses_SE3: List[npt.NDArray], 
+def mean_SE3_matrix(poses_SE3_matrix: List[npt.NDArray], 
                     conv_thresh=DEFAULT_CONV_THRESH, 
                     max_iters=DEFAULT_MAX_ITERS) -> npt.NDArray:
+    """Wrapper function for `mean` where the SE3 poses are given as 4x4 
+    matrices.
+    
+    :param poses_SE3_matrix: List of poses as 4x4 SE3 matrices
+    :param conv_thresh: Convergence threshold for norm of tangent vector
+    :param max_iters: Maximum iterations used in the algorithm
+    
+    :return: Mean pose as 4x4 SE3 matrix
+    """                
     poses = []
-    for T in poses_SE3:
+    for T in poses_SE3_matrix:
         pose = SE3.from_matrix(T)
         poses.append(pose)
 
     return mean(poses, conv_thresh, max_iters).to_matrix()
-
